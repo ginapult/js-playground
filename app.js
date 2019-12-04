324fc9b48e8f40db1cd50a6a9ce4195db3c33e3f
Gina Pultorak <gina.pultorak@gmail.com>
2019-12-04 11:51:03 -0800

    add app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log('yup, g! good to go on js!');
+
+var fruits = ['apple', 'banana', 'cantaloupe', 'dragonfruit'];
+
+for (var i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
