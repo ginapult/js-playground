f95c3b30f1d5a1348dbefa02b21a54132efbb35c
Gina Pultorak <gina.pultorak@gmail.com>
2019-12-04 11:54:08 -0800

    add while loop wehaveliftoff to app.js

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ var fruits = ['apple', 'banana', 'cantaloupe', 'dragonfruit'];
 for (var i = 0; i < fruits.length; i++) {
   console.log(fruits[i]);
 }
+
+var timeToLiftOff = 10;
+
+while (timeToLiftOff > 0) {
+  console.log(timeToLiftOff);
+  timeToLiftOff = timeToLiftOff - 1;
+}
+console.log('We have LIFTOFF!');
